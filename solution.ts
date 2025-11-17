46c6fe906a0991393afbd654ccd12d66580b74dc
mahbub-alam-dave <mahbubalamdave2.00@gmail.com>
2025-11-17 10:36:13 +0600

    solved three problems with appropriate solutions

--- solution.ts
+++ solution.ts
@@ -1,1 +1,44 @@
 // Beginning of next level courses assignment one
+const formatValue = (input : number | string | boolean) : number | string | boolean => {
+    if(typeof input === "number"){
+        return input * 10
+    } else if (typeof input === "string"){
+        return input.toUpperCase()
+    } else {
+        return input = !input
+    }
+}
+
+type ArrayInput = string[] | number[] | boolean[]
+const getLength = (input: string | ArrayInput) => {
+    if(typeof input === "string") {
+        return input.length
+    } else if (Array.isArray(input)) {
+        return input.length
+    }
+}
+
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number){
+        this.name = name;
+        this.age = age;
+    }
+
+    getDetails(){
+        return `Name: ${this.name}, Age: ${this.age}`
+    }
+}
+
+const person1 = new Person('John Doe', 30);
+const person2 = new Person('Alice', 25);
+
+type Items = {
+    title: string,
+    ratings: number
+}
+const filterByRating = (arrayOfItems: Items[]) => {
+
+}
